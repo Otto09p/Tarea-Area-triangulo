ae676dc73f0c38a399ace7d033a3baf52601f317
Fabián Arias <>
2021-04-22 23:25:07 +0000

    Comments

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ float altura; // Se configura la altura como flotante
 float resultado;
 
 
-cout <<"Calcule el area de un triangulo";
+cout <<"Calcule el area de un triangulo";// Se imprime el mensaje inicial hacia el usuario
 
 
-cout <<"\nDigite la altura: ";
+cout <<"\nDigite la altura: "; // El usuario digita la altura
 cin >>altura;
-cout <<"Digite la base del triangulo: ";
+cout <<"Digite la base del triangulo: "; //El usuario digita la base
 cin >>base;
-resultado= (base*altura)/2; 
+resultado= (base*altura)/2; // Se imprime la formula del área
 cout <<"El area del triangulo es: "<<resultado;
 
 return 0;
